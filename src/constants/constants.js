1052aac2f1ef8f04cc896517a76023ce66de44d1
Aswin Vijayakumar <aswinkvj@gmail.com>
2022-09-22 16:27:31 +0100

    react

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,9 @@
 const VIEW_LIST_S3_ITEM = "VIEW_LIST_S3_ITEM";
 const SELECT_S3_ITEM = "SELECT_S3_ITEM";
 const PROTO = "http";
-const HOST = "localhost";
+const HOST = "20.120.53.95";
 const PORT = 8080;
-const IMAGES_PATH = "http://localhost:8080/";
+const IMAGES_PATH = "http://20.120.53.95:8080/";
 const FILENAMES = [
     
 ];
